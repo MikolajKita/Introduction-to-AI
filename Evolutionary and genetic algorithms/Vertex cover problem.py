2846e89ba7911b6864f07fdade0fb208ee479064
mkita2 <m.t.kita@stud.elka.pw.edu.pl>
2021-03-29 22:29:19 +0200

    Added random symmetric graph method

--- Evolutionary and genetic algorithms/Vertex cover problem.py
+++ Evolutionary and genetic algorithms/Vertex cover problem.py
@@ -1,1 +1,9 @@
+import random
+import numpy as np
+
+
+def sym_graph(number_of_vertices):
+    graph = np.random.randint(1,3,size=(number_of_vertices,number_of_vertices))
+    graph_symm = (graph + graph.T)%2
+    return graph_symm
 
