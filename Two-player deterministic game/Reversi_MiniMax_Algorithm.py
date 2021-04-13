658eab55b8f69b84a644d5189a7f3ddddda9fe43
MikolajKita <mikolajkita@gmail.com>
2021-04-13 23:30:01 +0200

    Implemented improved rules of the game

--- Two-player deterministic game/Reversi_MiniMax_Algorithm.py
+++ Two-player deterministic game/Reversi_MiniMax_Algorithm.py
@@ -16,12 +16,14 @@ def in_board_boundaries(x_cor, x_direction, y_cor, y_direction):
         return False
 
 def valid_move(board_list, player, row_number, column_number):
-    number_to_flip_list = []
-    array_to_return = [0,0,0,0,0]
+
+    flip_list = []
+    return_array = []
 
     if board_list[row_number][column_number] != '+':
         #print('Znajduje sie tutaj figura')
-        return array_to_return
+        return_array = [False, 0]
+        return return_array
     if player == 1:
         seek_piece = -1
     else:
@@ -40,10 +42,20 @@ def valid_move(board_list, player, row_number, column_number):
                 y_cor = y_cor + y_direction
                 number_of_pieces = number_of_pieces + 1
         if board_list[x_cor][y_cor] == seek_piece*(-1) and number_of_pieces > 0:
-            array_to_return = [number_of_pieces, x_cor, y_cor, x_direction, y_direction]
-            return array_to_return
+            while not(x_cor == row_number and y_cor == column_number):
 
-    return array_to_return
+                x_cor = x_cor - x_direction
+                y_cor = y_cor - y_direction
+                flip_list.append([x_cor, y_cor])
+
+
+
+    if(len(flip_list)==0):
+        return_array = [False, 0]
+        return return_array
+    else:
+        return_array = [True, flip_list]
+        return return_array
 
 
 
@@ -53,7 +65,8 @@ def all_valid_moves(board_list, player, row=8, column=8):
         for j in range(0, column-1):
             array = valid_move(board_list, player, i, j)
             if array[0]:
-                valid_move_table[i][j] = array[0]
+
+                valid_move_table[i][j] = len(array) - 1
 
 
     board_array = np.array(valid_move_table, ndmin=2)
@@ -66,22 +79,13 @@ def move(row_number, column_number, board_list, player):
     else:
         value = -1
     helparray = valid_move(board_list, player, row_number, column_number)
-    x_end = helparray[1]
-    y_end = helparray[2]
-    x_direct = helparray[3]
-    y_direct = helparray[4]
-    #print(helparray)
-    if (x_direct == 0 and y_direct == 0):
-        new_board_array = np.array(board_list, ndmin=2)
-        return new_board_array
+    if(helparray[0]==True):
+        for i in helparray[1]:
+            x = i[0]
+            y = i[1]
+            board_list[x][y] = value
 
-    else:
-        board_list[row_number][column_number] = value
-        while not (row_number == x_end and y_end == column_number):
-            row_number = row_number + x_direct
-            column_number = column_number + y_direct
-            #print(x_direct, y_direc)
-            board_list[row_number][column_number] = value
+
 
     new_board_array = np.array(board_list, ndmin = 2)
     return new_board_array
@@ -99,14 +103,22 @@ def starting_moves(board_list):
     board_list[4][3] = -1
     board_list[4][4] = 1
 
-def random_move(available_moves, board_list, player):
+def is_a_move_possible(available_moves_array, row, column):
+    sumofmoves = 0
+    for i in range(0, row - 1):
+        for j in range(0, column - 1):
+            sumofmoves = sumofmoves + available_moves_array[i][j]
+    return sumofmoves
 
-    x = random.randint(0,7)
-    y = random.randint(0, 7)
+def random_move(available_moves, board_list, player, row, column):
+
+    if(is_a_move_possible(available_moves, row, column)==0):
+        return
+    x = random.randint(0,row-1)
+    y = random.randint(0, column-1)
     while available_moves[x][y] == 0:
-        x = random.randint(0, 7)
-        y = random.randint(0, 7)
-
+        x = random.randint(0, row-1)
+        y = random.randint(0, column-1)
     move(x, y, board_list, player)
 
 def score(board_list, row, column):
@@ -129,15 +141,20 @@ starting_moves(new_board_list)
 print_board(new_board_list)
 print(all_valid_moves(new_board_list, 1))
 i = 0
-while i < 5:
+while i < 1:
 
-    valid_moves = all_valid_moves(new_board_list,1)
-    random_move(valid_moves, new_board_list,1)
+    valid_moves = all_valid_moves(new_board_list,2)
+    move(3,2,new_board_list,2)
+    #random_move(valid_moves, new_board_list,1, 8, 8)
     print_board(new_board_list)
-
-    valid_moves = all_valid_moves(new_board_list, 2)
-    random_move(valid_moves, new_board_list,2)
+    valid_moves = all_valid_moves(new_board_list, 1)
+    move(4, 2, new_board_list, 1)
+    #random_move(valid_moves, new_board_list,2,8,8)
     print_board(new_board_list)
     i = i + 1
+    move(5, 2, new_board_list, 2)
+    print_board(new_board_list)
 
 score(new_board_list, 8,8)
+valid_moves = all_valid_moves(new_board_list, 1)
+print(valid_moves)
